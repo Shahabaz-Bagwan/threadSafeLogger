9c2fa6c7a036bbe325801d846e0bbb1bfd9a876a
Shahabaz Bagwan <4272320+Shahabaz-Bagwan@users.noreply.github.com>
2023-05-13 09:25:04 +0200

    Initial commit

---
+++ example/example1/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main( int argc, char* argv[] )
+{
+  using namespace std;
+  if( argc < 2 ) {
+    std::cout << "Please provide input argument" << std::endl;
+    return 1;
+  }
+
+  return 0;
+}
